da4492a17a9a27dfb1699d3b3cd884ed5c8d34a9
Rich Castagna <idev.tru@gmail.com>
2019-02-11 11:06:18 -0500

    Initial Commit

---
+++ ISpy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Richard Castagna on 2/11/19.
+//  Copyright © 2019 Richard Castagna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
